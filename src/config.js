df967884f6f2cc29b88d0355f946c686e2752a8c
Raul Carreras <raul.e.carreras@gmail.com>
2020-03-06 21:47:14 -0500

    a whole second commit

---
+++ src/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  API_TOKEN: process.env.API_TOKEN || '67e045f4-601d-11ea-bc55-0242ac130003',
+}
